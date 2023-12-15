35466c24ec80e2c00cbfdf58b9bdd75d8d31ff01
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2023-12-14 22:43:08 -0500

    add login checker

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,39 @@
+const { connectToDatabase, login } = require('../../databaseFunctions/db');
+
 exports.handler = async function () {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'LabSensors API is working'
-        })
+    const nameOfLab = "nia lab";
+    const passwordOfLab = "pi4life";
+
+    try {
+        const db = await connectToDatabase();
+
+        const lab = await login(db, nameOfLab, passwordOfLab);
+
+        if (lab.success) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    success: true,
+                    api: lab.api,
+                    message: 'Login successful'
+                })
+            };
+        } else {
+            return {
+                statusCode: 401,
+                body: JSON.stringify({
+                    message: 'Login unsuccessful'
+                })
+            };
+        }
+    } catch (err) {
+        console.error(err);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: 'Internal server error'
+            })
+        };
     }
-}
+};
