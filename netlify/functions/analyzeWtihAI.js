2c0c81d408dd149f5ebb50eab2ccdfec854d6292
bgilb33 <benjamingilbert302@gmail.com>
2024-01-05 19:21:07 -0500

    Added AI endpoint (pre-frontend split)

---
+++ netlify/functions/analyzeWtihAI.js
@@ -0,0 +1,33 @@
+const { headers, connectToDatabase, analyzeWithAI} = require('../../public/helpers/db');
+
+exports.handler = async function (event) {
+  const handleCors = (statusCode, body) => ({
+    statusCode,
+    headers: headers,
+    body: JSON.stringify(body),
+  });
+
+  // Handle preflight request
+  if (event.httpMethod === 'OPTIONS') {
+    return handleCors(200, {});
+  }
+
+  try {
+    const labApi = event.queryStringParameters.labApi;
+    const deviceID = event.queryStringParameters.deviceID;
+    const db = await connectToDatabase();
+    const response = await analyzeWithAI(db, labApi, deviceID);
+
+    if (response.success) {
+      return handleCors(200, response);
+    } else {
+      return handleCors(401, response);
+    }
+  } catch (err) {
+    console.error("Error:", err);
+
+    return handleCors(500, {
+      error: 'Internal server error',
+    });
+  }
+}
